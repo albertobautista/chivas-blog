5043e35fa50679fbadbcef9a0def1020a715e547
Alberto Bautista <dachb101@gmail.com>
2025-06-29 17:06:37 -0600

    Add next matches sections

---
+++ src/types/schedule.ts
@@ -0,0 +1,14 @@
+export interface ScheduleMatch {
+  home: {
+    name: string;
+    logo: string;
+  };
+  away: {
+    name: string;
+    logo: string;
+  };
+  date: string;
+  time: string;
+  stadium: string;
+  jornada: string;
+}
